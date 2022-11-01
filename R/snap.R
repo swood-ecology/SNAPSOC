52afd95b762b505db3ceeddcd3b1d099f546f985
Stephen Wood <stephenawood@gmail.com>
2022-11-01 13:12:17 -0400

    Update model functions to include option for no price data

--- R/snap.R
+++ R/snap.R
@@ -5,14 +5,16 @@
 #' @param nsamp (numeric) Number of samples to draw for bootstrapping procedure
 #' @param input_table Input data to use when using single values per variable
 #' @param plot_vars Which variables to include in plot of variable importance
+#' @param price (logical) Whether to include price data
 #'
 #' @return Output values for multiple variables
 #'
 #' @examples \dontrun{ article_pdf_download(infilepath = "/data/isi_searches", outfilepath = "data")}
 
-snap <- function(data=NA,bootstrap=FALSE,nsamp=10000,input_table=NA,plot_vars=NA){
+snap <- function(data=NA,bootstrap=FALSE,nsamp=10000,input_table=NA,plot_vars=NA,price=FALSE){
   if(bootstrap==TRUE){
-      mcSimulation_results <- decisionSupport::mcSimulation(
+    if(price==TRUE){
+        mcSimulation_results <- decisionSupport::mcSimulation(
         estimate = input_table,
         model_function = grazing_soc,
         numberOfModelRuns = nsamp, #run 1,000 times
@@ -22,13 +24,33 @@ snap <- function(data=NA,bootstrap=FALSE,nsamp=10000,input_table=NA,plot_vars=NA
         for(i in 1:length(plot_vars)){
           print(
             decisionSupport::plot_pls(decisionSupport::plsr.mcSimulation(object = mcSimulation_results,
-                                       resultName = names(mcSimulation_results$y)[which(names(mcSimulation_results$y)==plot_vars[i])],
-                                       ncomp = 1),
-                     threshold = 0)
+                                                                         resultName = names(mcSimulation_results$y)[which(names(mcSimulation_results$y)==plot_vars[i])],
+                                                                         ncomp = 1),
+                                      threshold = 0)
           )
         }
       }
       return(mcSimulation_results)
+    }
+    else if(price==FALSE){
+        mcSimulation_results <- decisionSupport::mcSimulation(
+        estimate = input_table,
+        model_function = grazing_soc_noprice,
+        numberOfModelRuns = nsamp, #run 1,000 times
+        functionSyntax = "plainNames"
+      )
+      if(!is.na(plot_vars)){
+        for(i in 1:length(plot_vars)){
+          print(
+            decisionSupport::plot_pls(decisionSupport::plsr.mcSimulation(object = mcSimulation_results,
+                                                                         resultName = names(mcSimulation_results$y)[which(names(mcSimulation_results$y)==plot_vars[i])],
+                                                                         ncomp = 1),
+                                      threshold = 0)
+          )
+        }
+      }
+      return(mcSimulation_results)
+    }
   }
   else{
     return(
