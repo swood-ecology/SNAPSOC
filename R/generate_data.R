9d4a4332be36b888cba3ef4e1724b3fd624c6dd3
Stephen Wood <stephenawood@gmail.com>
2022-11-01 22:36:04 -0400

    Rewrite without pipe operator

--- R/generate_data.R
+++ R/generate_data.R
@@ -10,32 +10,32 @@
 #' }
 generate_data <- function(data, obs = 1000) {
   mean_annual_rainfall <- rnorm(obs,
-    mean = data %>% filter(variable == "rain") %>% select("mean") %>% as.numeric(),
-    sd = data %>% filter(variable == "rain") %>% select("sd") %>% as.numeric()
+    mean = as.numeric(filter(data,variable == "rain")$"mean"),
+    sd = as.numeric(filter(data,variable == "rain")$"sd")
   )
   sand_percentage <- rnorm(obs,
-    mean = data %>% filter(variable == "sand") %>% select("mean") %>% as.numeric(),
-    sd = data %>% filter(variable == "sand") %>% select("sd") %>% as.numeric()
+    mean = as.numeric(filter(data,variable == "sand")$"mean"),
+    sd = as.numeric(filter(data,variable == "sand")$"sd")
   )
   fire_frequency <- rnorm(obs,
-    mean = data %>% filter(variable == "fire") %>% select("mean") %>% as.numeric(),
-    sd = data %>% filter(variable == "fire") %>% select("sd") %>% as.numeric()
+    mean = as.numeric(filter(data,variable == "fire")$"mean"),
+    sd = as.numeric(filter(data,variable == "fire")$"sd")
   )
   lignin_and_cellulose_content <- rnorm(obs,
-    mean = data %>% filter(variable == "lignin") %>% select("mean") %>% as.numeric(),
-    sd = data %>% filter(variable == "lignin") %>% select("sd") %>% as.numeric()
+    mean = as.numeric(filter(data,variable == "lignin")$"mean"),
+    sd = as.numeric(filter(data,variable == "lignin")$"sd")
   )
   soil_organic_cab_conc <- rnorm(obs,
-    mean = data %>% filter(variable == "soc") %>% select("mean") %>% as.numeric(),
-    sd = data %>% filter(variable == "soc") %>% select("sd") %>% as.numeric()
+    mean = as.numeric(filter(data,variable == "soc")$"mean"),
+    sd = as.numeric(filter(data,variable == "soc")$"sd")
   )
   grazing_intensity <- rnorm(obs,
-    mean = data %>% filter(variable == "grazing") %>% select("mean") %>% as.numeric(),
-    sd = data %>% filter(variable == "grazing") %>% select("sd") %>% as.numeric()
+    mean = as.numeric(filter(data,variable == "grazing")$"mean"),
+    sd = as.numeric(filter(data,variable == "grazing")$"sd")
   )
   bulk_density <- rnorm(obs,
-    mean = data %>% filter(variable == "bd") %>% select("mean") %>% as.numeric(),
-    sd = data %>% filter(variable == "bd") %>% select("sd") %>% as.numeric()
+    mean = as.numeric(filter(data,variable == "bd")$"mean"),
+    sd = as.numeric(filter(data,variable == "bd")$"sd")
   )
   return(tibble(mean_annual_rainfall, sand_percentage, fire_frequency, grazing_intensity, lignin_and_cellulose_content, soil_organic_cab_conc, bulk_density))
 }
